50ea568e2f9d6d5c8529db972c474ce0a5fda070
Sharon Oud <mrh_sharon@hotmail.com>
2017-05-12 23:10:16 -0500

    updating database for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,24 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+// for home
+// var connection = mysql.createConnection({
+//     port: 3306,
+//     host: "localhost",
+//     user: "root",
+//     password: "root",
+//     database: "burgers_db"
+// });
+
+
+
+//for heroku
 var connection = mysql.createConnection({
     port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "burgers_db"
+    host: "hngomrlb3vfq3jcr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "ug7r6ubsj0buncox",
+    password: "nt688ulmxb4q2qkd",
+    database: "sb1h23jua2ftbcuo"
 });
 
 // Make connection.
